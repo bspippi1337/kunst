2990928342abe32586c6108bd1c3a0b3ba841664
bspippi1337 <bspippi1337@gmail.com>
2026-07-26 01:19:42 +0200

    Version minimal gallery release as v5.2.0

--- blckswan-kunst/app/build.gradle.kts
+++ blckswan-kunst/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.blckswan.art"
         minSdk = 26
         targetSdk = 36
-        versionCode = 51
-        versionName = "5.1.0"
+        versionCode = 52
+        versionName = "5.2.0"
     }
 
     buildFeatures {
