cce564483f7dfe6db81e46b78da5111e7ca44f08
bspippi1337 <bspippi1337@gmail.com>
2026-07-26 00:45:36 +0200

    Bump BLCKSWAN KUNST to v5.0.0

--- blckswan-kunst/app/build.gradle.kts
+++ blckswan-kunst/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.blckswan.art"
         minSdk = 26
         targetSdk = 36
-        versionCode = 42
-        versionName = "4.2.0"
+        versionCode = 50
+        versionName = "5.0.0"
     }
 
     buildFeatures {
